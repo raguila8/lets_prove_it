01901b764e0f72223f36901f12189c2490b32f67
raguila8 <rodrigoaguilar887@gmail.com>
2018-10-01 19:09:36 -0700

    add user validation specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,145 @@ RSpec.describe User do
       expect(user.valid?).not_to be_truthy
       user.username = "rodrigo"
       expect(user.valid?).to be_truthy
-    end 
+    end
+
+    it "should not have an empty username" do
+      user.username = ""
+      expect(user.valid?).not_to be_truthy
+    end
+
+    it "should have a username of length at least 5" do
+      user.username = "a" * 4
+      expect(user.valid?).not_to be_truthy
+      user.username = "a" * 5
+      expect(user.valid?).to be_truthy
+    end
+
+    it "should have a username of length at most 18" do
+      user.username = "a" * 18
+      expect(user.valid?).to be_truthy
+      user.username = "a" * 19
+      expect(user.valid?).not_to be_truthy
+    end
+
+    it "should be unique" do
+      create(:user, username: "foobar")
+      user.username = "foobar"
+      expect(user.valid?).not_to be_truthy
+      user.username = "FOOBAR"
+      expect(user.valid?).not_to be_truthy    # username is case insensitive
+    end
+  end
+
+  describe "email" do
+    it "should not be nil" do
+      user.email = nil
+      expect(user.valid?).not_to be_truthy
+    end
+
+    it "should not be empty" do
+      user.email = ""
+      expect(user.valid?).not_to be_truthy
+    end
+
+    it "should not be longer than 255 characters" do
+      user.email = "a" * 244 + "@example.com"
+      expect(user.valid?).not_to be_truthy
+    end
+
+    it "should have valid addresses" do
+      valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+      valid_addresses.each do |valid_address|
+        user.email = valid_address
+        expect(user.valid?).to be_truthy, 
+             "#{valid_address.inspect} should be valid"
+      end
+    end
+
+    it "should not have invalid addresses" do
+      invalid_addresses = %w[user@example,com 
+                             user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+      invalid_addresses.each do |invalid_address|
+        user.email = invalid_address
+        expect(user.valid?).not_to be_truthy, 
+             "#{invalid_address.inspect} should be invalid"
+      end
+    end
+
+    it "should be unique" do
+      create(:user, email: "foobar@example.com")
+      user.email = "FOOBAR@EXAMPLE.COM"    # case insensitive
+      expect(user.valid?).not_to be_truthy 
+    end
+  end
+
+  describe "bio" do
+    it "should not be longer than 500" do
+      user.bio = "a" * 501
+      expect(user.valid?).not_to be_truthy 
+    end
+
+    it "should have a length less than or equal to 500" do
+      bio = "a"
+      10.times do
+        n = Random.rand(500) + 1
+        user.bio = bio * n
+        expect(user.valid?).to be_truthy,
+          "Bio length: #{n} should be less than or equal to 500"
+      end
+    end
+  end
+
+  describe "name" do
+    it "should not be longer than 70" do
+      user.name = "a" * 71
+      expect(user.valid?).not_to be_truthy 
+    end
+
+    it "should have a name less than or equal to 70" do
+      name = "a"
+      10.times do
+        n = Random.rand(70) + 1
+        user.name = name * n
+        expect(user.valid?).to be_truthy,
+          "Name length: #{n} should be less than or equal to 70"
+      end
+    end
+  end
+
+  describe "occupation" do
+    it "should not be longer than 70" do
+      user.occupation = "a" * 71
+      expect(user.valid?).not_to be_truthy 
+    end
+
+    it "should have a length less than or equal to 70" do
+      occupation = "a"
+      10.times do
+        n = Random.rand(70) + 1
+        user.occupation = occupation * n
+        expect(user.valid?).to be_truthy,
+          "Occupation length: #{n} should be less than or equal to 70"
+      end
+    end
+  end
+
+  describe "education" do
+    it "should not be longer than 70" do
+      user.education = "a" * 71
+      expect(user.valid?).not_to be_truthy 
+    end
+
+    it "should have a length less than or equal to 70" do
+      education = "a"
+      10.times do
+        n = Random.rand(70) + 1
+        user.education = education * n
+        expect(user.valid?).to be_truthy,
+          "Education length: #{n} should be less than or equal to 70"
+      end
+    end
   end
 end
